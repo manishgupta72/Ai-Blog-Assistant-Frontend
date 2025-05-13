78f9049f205a34fbeb95923c0b95d489e716b027
Manish Gupta <manishgupta724872@gmail.com>
2025-05-13 14:02:37 +0530

    fixed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,17 +4,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
+    Authorization: process.env.NEXT_PUBLIC_API_TOKEN || "",
     "Content-Type": "application/json",
   },
-});
-
-// 🔐 Attach Authorization header from env token
-instance.interceptors.request.use((config) => {
-  const token = process.env.NEXT_PUBLIC_API_TOKEN;
-  if (token) {
-    config.headers.Authorization = token;
-  }
-  return config;
 });
 
 export default instance;
